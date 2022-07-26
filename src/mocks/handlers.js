28e72b91f3ab2f79d77275ea4e8a08113f9efce7
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2022-07-26 16:46:32 +0200

    build: mock getPeople1

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,8 +1,9 @@
 // handlers.js
 import { rest } from "msw";
+import getPeople1 from "./fixtures/getPeople1.json";
+
 export const handlers = [
-  rest.get("/quote", {
-    an: "awesome",
-    axa: "quote",
+  rest.get("https://swapi.dev/api/people/1", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(getPeople1));
   }),
 ];
