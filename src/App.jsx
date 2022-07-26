eca6e3c4850af4af8b8509552480b1b90ac08ef9
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2022-07-26 16:41:51 +0200

    feat: fetch basique

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/1").then((res) => {
+      res.json().then((data) => setName(data.name));
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
     </div>
   );
-}
+};
 
 export default App;
