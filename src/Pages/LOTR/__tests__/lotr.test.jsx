f55cd32c83a821081456aed7e870c2f23eebb929
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2024-01-16 09:55:12 +0100

    test(lotr): test mick mock

--- src/Pages/LOTR/__tests__/lotr.test.jsx
+++ src/Pages/LOTR/__tests__/lotr.test.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { describe, expect, it } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { http, HttpResponse, delay } from "msw";
 import { MemoryRouter } from "react-router-dom";
 import { LOTR } from "../LOTR";
 import { server } from "../../../mocks/server";
@@ -9,13 +10,14 @@ import { handlers } from "../../../mocks/handlers";
 // Tests
 describe("Renders main page correctly", async () => {
   it("Should render the page correctly", async () => {
-    server.use(handlers.getCharacter200);
-    render(
-      <MemoryRouter initialEntries={["/lotr"]}>
-        <LOTR />
-      </MemoryRouter>
+    server.use(
+      http.get("https://the-one-api.dev/v2/character", async () => {
+        await delay(1000);
+        return HttpResponse.json(getCharacter, { status: 200 });
+      })
     );
+    render(<LOTR />);
     expect(screen.getByText("Loading")).toBeInTheDocument();
-    expect(await screen.findByText("Amandil")).toBeInTheDocument();
+    expect(await screen.findByText("Amrod")).toBeInTheDocument();
   });
 });
